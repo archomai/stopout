50cb58bba9e248b21840a14c3a9411ee60512fa5
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-04 22:14:39 +0900

    admin.py 설정 및 views.py render 설정

--- django/webtoon/views.py
+++ django/webtoon/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Webtoon
+from .models import Webtoon, Episode
 
 
 def webtoon_list(request):
-    return HttpResponse('list')
+
+    webtoon = Webtoon.objects.all()
+    context = {
+        'webtoon': webtoon,
+    }
+    return render(
+        request=request,
+        template_name='blog/post_list.html',
+        context=context,
+    )
 
 
-def webtoon_detail(request, pk):
-    return HttpResponse('detail')
+def webtoon_detail(request, episode_id):
+
+    episode = Episode.objects.get(pk=episode_id)
+    context = {
+        'episode': episode,
+    }
+    return render(
+        request=request,
+        context=context,
+    )
